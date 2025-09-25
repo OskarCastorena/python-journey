8c430138c8726cfef05ce62d648a74fe54e6c384
OskarCastorena <oskarcastorena@icloud.com>
2025-09-25 12:01:43 -0600

    Added a loop until the user decide to stop, move the input promt after the user operation decision.

--- fundamentals/basic_console_calculator/calculator.py
+++ fundamentals/basic_console_calculator/calculator.py
@@ -2,32 +2,39 @@ import pyfiglet
 
 print(pyfiglet.figlet_format('CALCULATOR', font='big'))
 
-a = int(input("Please enter a number: "))
-b = int(input("Please enter another number: "))
+decision = 'y'
 
-operation = input('''
-which operation would you like to perform?
+while decision == 'y':
 
-+---+----------------+
-| + | Addition       |
-| - | Subtraction    |
-| * | Multiplication |
-| / | Division       |
-+---+----------------+
 
-Enter the symbol:
-''')
 
-if operation == '+':
-    print(f"the sum of {a} and {b} is {a + b}")
-elif operation == '-':
-    print(f"the subtraction of {a} and {b} is {a - b}")
-elif operation == '*':
-    print(f"the multiplication of {a} and {b} is {a * b}")
-elif operation == '/':
-    if a == 0 or b == 0:
-        print("It is impossible to calculate a division by zero")
+    operation = input('''
+    which operation would you like to perform?
+    
+    +---+----------------+
+    | + | Addition       |
+    | - | Subtraction    |
+    | * | Multiplication |
+    | / | Division       |
+    +---+----------------+
+    
+    Enter the symbol:
+    ''')
+    a = int(input("Please enter a number: "))
+    b = int(input("Please enter another number: "))
+
+    if operation == '+':
+        print(f"the sum of {a} and {b} is {a + b}")
+    elif operation == '-':
+        print(f"the subtraction of {a} and {b} is {a - b}")
+    elif operation == '*':
+        print(f"the multiplication of {a} and {b} is {a * b}")
+    elif operation == '/':
+        if a == 0 or b == 0:
+            print("It is impossible to calculate a division by zero")
+        else:
+            print(f"the division of {a} and {b} is {a / b}")
     else:
-        print(f"the division of {a} and {b} is {a / b}")
-else:
-    print("Invalid operation")
+        print("Invalid operation")
+
+    decision = input("Do you want to continue? (y/n)")
